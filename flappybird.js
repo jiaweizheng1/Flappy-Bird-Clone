f5f4f0cc7d8501fc468ad272bc4132da02a621ed
jiaweizheng1 <jiaweizheng416528@gmail.com>
2023-09-12 19:26:46 -0700

    Functioning Game With Sounds
    Missing: Scoreboard
    Missing: Bird fall animation on hitting pipe

--- flappybird.js
+++ flappybird.js
@@ -10,17 +10,34 @@ let bird =
     width : 34,
     height : 24,
     velocityY: 0,
-    tiltAngle: 0 
+    tiltAngle: 0,
+}
+
+//base class; make base width 2x larger for simulating base moving to left
+let base =
+{
+    x : 0,
+    width:  336*2,
+    height: 112,
+    velocityX: -0.75
 }
 
 //pipes array
 let pipeArray = []; 
 
 //physics
-let gravity = 0.025;
+let gravity = 0.05;
 
 //volume control
-volume = 0.5;
+volume = 0.35;
+const birdJumpAud = new Audio("audio/wing.wav");
+birdJumpAud.volume = volume;
+const gameOverAud = new Audio("audio/hit.wav");
+gameOverAud.volume = volume;
+const pointAud = new Audio("audio/point.wav");
+gameOverAud.volume = volume;
+
+let gameOver = false;
 
 window.onload = function() 
 {
@@ -66,6 +83,16 @@ function update()
 {
     requestAnimationFrame(update);
 
+    if(gameOver)
+    {
+        pipeArray = [];
+        bird.x = board.width*3/8;
+        bird.y = board.height/2;
+        bird.velocityY = 0;
+
+        return;
+    }
+
     //clear previous pixels to allow for drawing new images
     context.clearRect(0, 0, board.width, board.height);
 
@@ -75,19 +102,69 @@ function update()
         let pipe = pipeArray[i];
         pipe.x += pipe.velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        if(detectcollision(bird, pipe))
+        {
+            gameOver = true;
+            gameOverAud.play();
+        }
+
+        if(pipe.passed == false && bird.x > pipe.x)
+        {
+            pointAud.play();
+            pipe.passed = true;
+        }
+    }
+
+    if(bird.y + bird.height > board.height - base.height)
+    {
+        gameOver = true;
+        gameOverAud.play();
     }
 
     //calculate new bird y position after jumping + gravity
-    bird.velocityY += gravity*2;
-    bird.y += bird.velocityY;
+    bird.velocityY += gravity;
+    //cap bird from going too much out of top of the screen
+    bird.y = Math.max(bird.y + bird.velocityY, -25);
+
+     // Accelerate the tilt angle upwards when jumping
+    if (bird.velocityY < 0) {
+        bird.tiltAngle -= 1; // Increase tilt angle upwards
+        if (bird.tiltAngle < -25) { // Limit the maximum tilt angle
+            bird.tiltAngle = -25;
+        }
+    }
+
+    // Rotate the bird downward if it's falling
+    if (bird.velocityY > 0) {
+        bird.tiltAngle += 1; // Increase tilt angle downwards
+        if (bird.tiltAngle > 90) { // Limit the maximum tilt angle
+            bird.tiltAngle = 90;
+        }
+    }
 
     //draw bird
     context.save();
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    context.translate(bird.x + bird.width / 2, bird.y + bird.height / 2);
+    context.rotate((Math.PI / 180) * bird.tiltAngle);
+    context.drawImage(birdImg, -bird.width / 2, -bird.height / 2, bird.width, bird.height);
     context.restore();
 
+    base.x += base.velocityX;
+
+    if(base.x <= -board.width)
+    {
+        base.x = 0;
+    }
+
     //draw base
-    context.drawImage(baseImg, 0, board.height*25/32, baseImg.width, baseImg.height)
+    context.drawImage(baseImg, base.x, board.height*25/32, base.width, base.height)
+
+    //clear pipes
+    while(pipeArray.length > 0 && pipeArray[0].x < -board.width/2)
+    {
+        pipeArray.shift();
+    }
 }
 
 function placePipes() 
@@ -127,11 +204,19 @@ function birdJump(b)
     //button 0 is left mouse click; key code 32 is spacebar
     if(b.keyCode == 32 || b.button == 0)
     {
-        const birdJumpAud = new Audio("audio/wing.wav");
-
-        birdJumpAud.volume = volume;
+        gameOver = false;
+        
         birdJumpAud.play();
     
+        bird.tiltAngle = -65
         bird.velocityY = -2.4;
     }
+}
+
+function detectcollision(a, b)
+{
+    return a.x < b.x + b.width &&
+        a.x + a.width > b.x &&
+        a.y < b.y + b.height &&
+        a.y + a.height > b.y;
 }
