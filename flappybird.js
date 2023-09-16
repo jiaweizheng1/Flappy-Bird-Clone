6b5f04e33822cdee3bb154f75e87673fa21791fc
jiaweizheng1 <jiaweizheng416528@gmail.com>
2023-09-16 01:20:25 -0700

    Score Done

--- flappybird.js
+++ flappybird.js
@@ -11,7 +11,7 @@ let bird =
     height : 24,
     velocityY: 0,
     tiltAngle: 0,
-}
+};
 
 //base class; make base width 2x larger for simulating base moving to left
 let base =
@@ -20,7 +20,7 @@ let base =
     width:  336*2,
     height: 112,
     velocityX: -0.75
-}
+};
 
 //pipes array
 let pipeArray = []; 
@@ -29,7 +29,7 @@ let pipeArray = [];
 let gravity = 0.05;
 
 //volume control
-volume = 0.35;
+const volume = 0.35;
 const birdJumpAud = new Audio("audio/wing.wav");
 birdJumpAud.volume = volume;
 const gameOverAud = new Audio("audio/hit.wav");
@@ -39,12 +39,23 @@ gameOverAud.volume = volume;
 
 let gameOver = false;
 
+//index for selecting bird flap animation
+let flapFrame = 0;
+
+//bird images animation array
+let birdImgs = [];
+
+//numbers array for score
+let numbers = [];
+
+let score = 0;
+
 window.onload = function() 
 {
     //load night background image
     //uncessary but may be needed when adding day->night, night->day cycle
     backgroundImg = new Image();
-    backgroundImg.src = "sprites/background-night.png"
+    backgroundImg.src = "sprites/background-night.png";
 
     //context for drawing on board
     board = document.getElementById("board");
@@ -52,29 +63,53 @@ window.onload = function()
     board.height = backgroundImg.height;
     context = board.getContext("2d");
 
-    //bird image
+    //numbers images
+    for (let i = 0; i < 10; i++)
+    {
+        numberImg = new Image();
+        numberImg.src = `sprites/${i}.png`;
+        numbers.push(numberImg);
+    }
+
+    //intro message
+    introImg = new Image();
+    introImg.src = "sprites/message.png";
+
+    //bird flap image 1
     birdImg = new Image();
-    birdImg.src = "sprites/yellowbird-midflap.png"
+    birdImg.src = "sprites/yellowbird-downflap.png";
+    birdImgs.push(birdImg);
+
+    //bird flap image 2
+    birdImg2 = new Image();
+    birdImg2.src = "sprites/yellowbird-midflap.png";
+    birdImgs.push(birdImg2);
+
+    //bird flap image 3
+    birdImg3 = new Image();
+    birdImg3.src = "sprites/yellowbird-upflap.png";
+    birdImgs.push(birdImg3);
 
     //place bird at starting position
-    bird.x = board.width*3/8;
+    bird.x = board.width*5/16;
     bird.y = board.height/2;
 
     //base image
     baseImg = new Image();
-    baseImg.src = "sprites/base.png"
+    baseImg.src = "sprites/base.png";
 
     //top pipe image
     toppipeImg = new Image();
-    toppipeImg.src = "sprites/pipe-green-top.png"
+    toppipeImg.src = "sprites/pipe-green-top.png";
 
     //bot pipe image
     botpipeImg = new Image();
-    botpipeImg.src = "sprites/pipe-green-bot.png"
+    botpipeImg.src = "sprites/pipe-green-bot.png";
 
     //start animation, draw new pipes, bird jump when key press
     requestAnimationFrame(update);
-    setInterval(placePipes, 1.5 * 1000) //every 1.5 seconds
+    setInterval(placePipes, 1.5 * 1000); //every 1.5 seconds
+    setInterval(flap, 0.1 * 1000);
     document.addEventListener("mousedown", birdJump);
     document.addEventListener("keydown", birdJump);
 }
@@ -85,10 +120,16 @@ function update()
 
     if(gameOver)
     {
+        //draw intro message
+        context.drawImage(introImg, backgroundImg.width/2-introImg.width/2, backgroundImg.height/2-introImg.height/2);
+
+        //reset pipes and bird
         pipeArray = [];
-        bird.x = board.width*3/8;
         bird.y = board.height/2;
         bird.velocityY = 0;
+
+        //reset score
+        score = 0;
 
         return;
     }
@@ -111,11 +152,24 @@ function update()
 
         if(pipe.passed == false && bird.x > pipe.x)
         {
+            score += 0.5;
             pointAud.play();
             pipe.passed = true;
         }
     }
 
+    //draw score
+    let scoreCopy = score;
+    var xpos = backgroundImg.width/2 + numbers[0].width*(score.toString().length*0.5 - 1);
+    for (let i = 0; i < score.toString().length; i++)
+    {
+        let remainder = scoreCopy % 10; 
+        context.drawImage(numbers[remainder], xpos, backgroundImg.height/5);
+        xpos -= numbers[0].width;
+        scoreCopy = Math.floor(scoreCopy/10);
+    }
+
+    //check if bird hit the ground
     if(bird.y + bird.height > board.height - base.height)
     {
         gameOver = true;
@@ -124,21 +178,25 @@ function update()
 
     //calculate new bird y position after jumping + gravity
     bird.velocityY += gravity;
-    //cap bird from going too much out of top of the screen
+    //prevent bird from going too much out of top of the screen
     bird.y = Math.max(bird.y + bird.velocityY, -25);
 
      // Accelerate the tilt angle upwards when jumping
-    if (bird.velocityY < 0) {
+    if (bird.velocityY < 0) 
+    {
         bird.tiltAngle -= 1; // Increase tilt angle upwards
-        if (bird.tiltAngle < -25) { // Limit the maximum tilt angle
+        if (bird.tiltAngle < -25) 
+        { // Limit the maximum tilt angle
             bird.tiltAngle = -25;
         }
     }
 
     // Rotate the bird downward if it's falling
-    if (bird.velocityY > 0) {
+    if (bird.velocityY > 0) 
+    {
         bird.tiltAngle += 1; // Increase tilt angle downwards
-        if (bird.tiltAngle > 90) { // Limit the maximum tilt angle
+        if (bird.tiltAngle > 90) 
+        { // Limit the maximum tilt angle
             bird.tiltAngle = 90;
         }
     }
@@ -147,11 +205,11 @@ function update()
     context.save();
     context.translate(bird.x + bird.width / 2, bird.y + bird.height / 2);
     context.rotate((Math.PI / 180) * bird.tiltAngle);
-    context.drawImage(birdImg, -bird.width / 2, -bird.height / 2, bird.width, bird.height);
+    context.drawImage(birdImgs[flapFrame], -bird.width / 2, -bird.height / 2, bird.width, bird.height);
     context.restore();
 
+    //simulate base moving left
     base.x += base.velocityX;
-
     if(base.x <= -board.width)
     {
         base.x = 0;
@@ -160,7 +218,7 @@ function update()
     //draw base
     context.drawImage(baseImg, base.x, board.height*25/32, base.width, base.height)
 
-    //clear pipes
+    //clear pipes that are out of screen
     while(pipeArray.length > 0 && pipeArray[0].x < -board.width/2)
     {
         pipeArray.shift();
@@ -169,8 +227,13 @@ function update()
 
 function placePipes() 
 {
-    let randompipeY = -320/4 - Math.random()*(320/2)
-    let spacing = board.height/5
+    if (gameOver)
+    {
+        return;
+    }
+
+    let randompipeY = -320/4 - Math.random()*(320/2);
+    let spacing = board.height/5;
 
     let topPipe = 
     {
@@ -181,9 +244,9 @@ function placePipes()
         height: 320,
         velocityX: -0.75,
         passed : false
-    }
+    };
 
-    pipeArray.push(topPipe)
+    pipeArray.push(topPipe);
 
     let botPipe = 
     {
@@ -194,18 +257,18 @@ function placePipes()
         height: 320,
         velocityX: -0.75,
         passed : false
-    }
+    };
 
-    pipeArray.push(botPipe)
+    pipeArray.push(botPipe);
 }
 
 function birdJump(b)    
 {
     //button 0 is left mouse click; key code 32 is spacebar
-    if(b.keyCode == 32 || b.button == 0)
+    if(b.button == 0 || b.keyCode == 32)
     {
         gameOver = false;
-        
+
         birdJumpAud.play();
     
         bird.tiltAngle = -65
@@ -213,6 +276,17 @@ function birdJump(b)
     }
 }
 
+function flap()
+{
+    if (gameOver)
+    {
+        return;
+    }
+
+    flapFrame = (flapFrame + 1) % 3;
+}
+
+//function to check for collision between two objects
 function detectcollision(a, b)
 {
     return a.x < b.x + b.width &&
